e38796eacb46b7f629487f2ec67a0e3bf8303ab6
root <root@deb1.iftac.se>
2019-01-14 15:36:23 +0100

    Update info.sh

--- info.sh
+++ info.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-lokalIP=$(ip address | grep inet | grep enp0s3 | cut -d " " -f 6 | cut -d "/" -f 1)
+lokalIP=$(ip address | grep inet | grep eth0 | cut -d " " -f 6 | cut -d "/" -f 1)
 dist=$(cat /etc/*-release | grep -w "NAME" | cut -d '"' -f 2)
 distVer=$(cat /etc/*-release | grep -w VERSION | cut -d '"' -f 2)
 kernelVer=$(uname -a | cut -d ' ' -f 3)
-lastBoot=$(uptime | cut -d ' ' -f 4 | sed 's/\,//')
+lastBoot=$(uptime | cut -d ' ' -f 5 | sed 's/\,//')
 loggedInUsers=$(users)
 	
 echo "Hostname: `hostname`"
